4a94af09d1c3b1b001b0e9385e429c9b2795a33b
David Márquez Delgado <d.marquez.delgado.91@gmail.com>
2026-02-03 16:24:33 +0100

    Initial commit

--- ddd4d-blog/content/introduction/_index.md
+++
@@ -1,7 +0,0 @@
-+++
-title = "Introduction"
-type = "chapter"
-weight = 1
-+++
-
-This is a new chapter.
--- ddd4d-blog/public/searchindex.en.js
+++ ddd4d-blog/public/searchindex.en.js
@@ -1,26 +1,18 @@
 var relearn_searchindex = [
   {
-    "breadcrumb": "Domain Driven Design for Dummies",
-    "content": "This is a new chapter.",
-    "description": "This is a new chapter.",
-    "tags": [],
-    "title": "Introduction",
-    "uri": "/ddd4d.github.io/introduction/index.html"
-  },
-  {
-    "breadcrumb": "Domain Driven Design for Dummies",
+    "breadcrumb": "Domain Driven Design 4 D",
     "content": "",
     "description": "",
     "tags": [],
     "title": "Categories",
-    "uri": "/ddd4d.github.io/categories/index.html"
+    "uri": "/ddd4d-blog/categories/index.html"
   },
   {
-    "breadcrumb": "Domain Driven Design for Dummies",
+    "breadcrumb": "Domain Driven Design 4 D",
     "content": "",
     "description": "",
     "tags": [],
     "title": "Tags",
-    "uri": "/ddd4d.github.io/tags/index.html"
+    "uri": "/ddd4d-blog/tags/index.html"
   }
 ]
